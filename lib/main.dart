015bfae0bd40b2cbb79e31c7f6856c665ef6788b
gosteve <nomuojamaro@gmail.com>
2020-04-28 15:36:56 +0100

    completed the app added functionality -flat button to make the ball clickable -changes the ball randomly when clicked enabled by the random() class of the dart:math library -created a function  changeBall to track changes in the states

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() =>
+    runApp(MaterialApp(
+      home: BallPage(),
+    ));
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  void changeBall() {
+    setState(() {
+      ballState = Random().nextInt(4) + 1;
+    });
+  }
+
+  int ballState = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Center(
+        child: Container(
+          child: Image.asset('images/ball$ballState.png'),
+        ),
+      ),
+      onPressed: () {
+        print(ballState);
+        changeBall();
+      },
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Ball(),
+      backgroundColor: Colors.blue[500],
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
       ),
     );
+  }
+}
